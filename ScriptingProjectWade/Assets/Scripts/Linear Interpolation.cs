ce22f9ca38b793b4604d5c3623e95288458678c8
Jeremiah00 <jeremiah.w.wade567@gmail.com>
2023-02-09 10:35:30 -0800

    yes

---
+++ ScriptingProjectWade/Assets/Scripts/Linear Interpolation.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{/*
+    float result = Mathf.Lerp(3f, 5f, 0.5f);
+    //Mathf.Lerp takes 3 floats one tells it where to start one tells them where to stop, and the last one tells it how far to interpolate/step
+
+
+    Vector3 from = new Vector3(1f, 2f, 3f);
+    Vector3 to = new Vector3(5f, 6f, 7f);
+
+    Vector3 result = Vector3.Lerp(from, to, 0.75f);
+    //Vector3.Lerp does the same thing as Mathf.Lerp but the from and to values are of type Color and Vector3. The third parameter in the same.
+    // Start is called before the first frame update
+    void Start()
+    { This one changve light intensity every frame 
+        lignt.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    { The one changes light intensity every second
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+    }
+
+*/}
+
