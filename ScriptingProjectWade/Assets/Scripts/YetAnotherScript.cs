5d99ce4658f05f5ca761e01d755aeaed149ecd47
Jeremiah00 <jeremiah.w.wade567@gmail.com>
2023-03-15 11:41:43 -0700

    m
    m

---
+++ ScriptingProjectWade/Assets/Scripts/YetAnotherScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YetAnotherScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int numberOFPlayerDeaths = 3;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
