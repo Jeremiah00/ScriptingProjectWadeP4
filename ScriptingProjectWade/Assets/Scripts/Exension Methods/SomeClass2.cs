1ee08fe8d0b26b5c277aa5a9e00b6b01c3a3ce8f
Jeremiah00 <jeremiah.w.wade567@gmail.com>
2024-02-20 11:39:45 -0800

    stuff

---
+++ ScriptingProjectWade/Assets/Scripts/Exension Methods/SomeClass2.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeClass2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        transform.ResetTransformation();
+
+    }
+
+    
+}
